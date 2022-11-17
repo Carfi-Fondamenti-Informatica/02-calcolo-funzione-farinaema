fe9d6479ca7128945a47dd97fde39a689adacba6
farinaema <117036663+farinaema@users.noreply.github.com>
2022-11-17 16:28:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main(){
     if (x<0 && y>0){
         z=(a*x)-(b*y);
     } else if ( x>=0 && y<=0){
-        z=(a*(x*x))-(b*(y*y));
+        z=a*(x*x)-b*(y*y);
     } else {
-        z=(a*x)-(b*(y*y));
+        z=(a*x)-b*(y*y);
     }
     cout << z;
 }
