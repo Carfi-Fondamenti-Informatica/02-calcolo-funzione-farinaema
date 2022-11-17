263803153fd4e39b23b0806c32357566f3781701
farinaema <117036663+farinaema@users.noreply.github.com>
2022-11-17 16:34:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,21 @@ int main(){
 
     float a, b, x, y, z;
     cin >> a >> b >> x >> y;
-
+    
     if (x<0 && y>0){
+        
         z=(a*x)-(b*y);
+        
     } else if ( x>=0 && y<=0){
-        z=a*(x*x)-b*(y*y);
+        
+        z=(a*(x*x))-(b*(y*y));
+    
     } else {
-        z=(a*x)-b*(y*y);
+        
+        z=(a*x)-(b*(y*y));
     }
-    cout << z;
+    
+    cout << z << endl;
+    
+    return 0;
 }
